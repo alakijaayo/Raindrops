7b0f7d7bfbee0479390ba527e62df8dcb169bccb
alakijaayo <alakijaayo@gmail.com>
2019-02-15 19:12:17 +0000

    Changing tests and methods
    Have made some changes to my tests and to the methods. Have made all tests become equal to true, and have changed the methods by putting a ? in from of them. Have done this so as to make the methods return true or false boolean statements, as my plan for trying to refactor this and keep the methods would require me to do this. Tests still pass, and will now try and refactor this into one method that checks all numbers.

--- lib/factor.rb
+++ lib/factor.rb
@@ -1,34 +1,34 @@
 class Factor
 
-  def pling(n)
-    return "Pling" if n % 3 == 0
+  def pling?(n)
+    n % 3 == 0
   end
 
-  def plang(n)
-    return "Plang" if n % 5 == 0
+  def plang?(n)
+    n % 5 == 0
   end
 
-  def plong(n)
-    return "Plong" if n % 7 == 0
+  def plong?(n)
+    n % 7 == 0
   end
 
-  def plingPlang(n)
-    return "PlingPlang" if n % 15 == 0
+  def plingPlang?(n)
+    n % 15 == 0
   end
 
-  def plingPlong(n)
-    return "PlingPlong" if n % 21 == 0
+  def plingPlong?(n)
+    n % 21 == 0
   end
 
-  def plangPlong(n)
-    return "PlangPlong" if n % 35 == 0
+  def plangPlong?(n)
+    n % 35 == 0
   end
 
-  def plingPlangPlong(n)
-    return "PlingPlangPlong" if n % 105 == 0
+  def plingPlangPlong?(n)
+    n % 105 == 0
   end
 
-  def number(n)
-    return "#{n}" if n % 3 != 0 && n % 5 != 0 && n % 7 != 0
+  def number?(n)
+    n % 3 != 0 && n % 5 != 0 && n % 7 != 0
   end
 end
--- spec/factor_spec.rb
+++ spec/factor_spec.rb
@@ -3,35 +3,35 @@ require './lib/factor'
 describe Factor do
 
   it "returns the word 'Pling' if number is divisible by 3" do
-    expect(subject.pling(6)).to eq("Pling")
+    expect(subject.pling?(6)).to eq(true)
   end
 
   it "returns the word 'Plang' if number is divisible by 5" do
-    expect(subject.plang(10)).to eq("Plang")
+    expect(subject.plang?(10)).to eq(true)
   end
 
   it "returns the word 'Plong' if number is divisible by 7" do
-    expect(subject.plong(14)).to eq("Plong")
+    expect(subject.plong?(14)).to eq(true)
   end
 
   it "returns the word 'PlingPlang' if number is divisible by 3 and 5" do
-    expect(subject.plingPlang(30)).to eq("PlingPlang")
+    expect(subject.plingPlang?(30)).to eq(true)
   end
 
   it "returns the word 'PlingPlong' if number is divisible by 3 and 7" do
-    expect(subject.plingPlong(42)).to eq("PlingPlong")
+    expect(subject.plingPlong?(42)).to eq(true)
   end
 
   it "returns the word 'PlangPlong' if number is divisible by 5 and 7" do
-    expect(subject.plangPlong(70)).to eq("PlangPlong")
+    expect(subject.plangPlong?(70)).to eq(true)
   end
 
   it "returns the word 'PlingPlangPlong' if number is divisible by 3, 5 and 7" do
-    expect(subject.plingPlangPlong(210)).to eq("PlingPlangPlong")
+    expect(subject.plingPlangPlong?(210)).to eq(true)
   end
 
   it "returns the number in string form if number is not divisible by 3, 5 or 7" do
-    expect(subject.number(34)).to eq("34")
+    expect(subject.number?(34)).to eq(true)
   end
 
 end
