7a848645c1e5f9dd3d4e8b3bfdd0b5bb23e5682c
alakijaayo <alakijaayo@gmail.com>
2019-02-15 15:17:51 +0000

    Testing Plong
    Writing a test that checks if the number is divisible by 7 and returns 'Plong'. Test Currently fails

--- spec/factor_spec.rb
+++ spec/factor_spec.rb
@@ -9,4 +9,8 @@ describe Factor do
   it "returns the word 'Plang' if number divisible by 5" do
     expect(subject.plang(10)).to eq("Plang")
   end
+
+  it "returns the word 'Plong' if number divisible by 7" do
+    expect(subject.plong(14)).to eq("Plong")
+  end
 end
