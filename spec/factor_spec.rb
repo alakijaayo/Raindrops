fa54207d9c53d58a3dafebf49f56d569fd218d9f
alakijaayo <alakijaayo@gmail.com>
2019-02-15 16:04:24 +0000

    Testing 'PlangPlong'
    Writing a test to check if the number is divisible by 5 and 7 and should return 'PlangPlong'. Test currently fails

--- spec/factor_spec.rb
+++ spec/factor_spec.rb
@@ -14,11 +14,16 @@ describe Factor do
     expect(subject.plong(14)).to eq("Plong")
   end
 
-  it "returns the word 'PlingPlang' if number is divisble by 3 and 5" do
+  it "returns the word 'PlingPlang' if number is divisible by 3 and 5" do
     expect(subject.plingPlang(30)).to eq("PlingPlang")
   end
 
   it "returns the word 'PlingPlong' if number is divisible by 3 and 7" do
     expect(subject.plingPlong(42)).to eq("PlingPlong")
   end
+
+  it "returns the word 'PlangPlong' if number is divisible by 5 and 7" do
+    expect(subject.plangPlong(70)).to eq("PlangPlong")
+  end
+
 end
