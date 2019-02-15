a47979d155de6daaa3c1fbc67f9acf2ed6bbf9aa
alakijaayo <alakijaayo@gmail.com>
2019-02-15 15:10:19 +0000

    Testing for Plang
    Writing the test to check if the number is divisible for 5. Currently the test fails, will write method to pass this next

--- spec/factor_spec.rb
+++ spec/factor_spec.rb
@@ -5,4 +5,8 @@ describe Factor do
   it "returns the word 'Pling' if number divisible by 3" do
     expect(subject.pling(6)).to eq("Pling")
   end
+
+  it "returns the word 'Plang' if number divisible by 5" do
+    expect(subject.plang(10)).to eq("Plang")
+  end
 end
