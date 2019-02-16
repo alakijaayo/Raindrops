4010d16d5b27d92bc062a77cf6c84fb5ade27773
alakijaayo <alakijaayo@gmail.com>
2019-02-16 10:07:07 +0000

    Refactoring
    Refactored code to make my code much more readable and get rid of the countless if and else statements as that is not clean coding. Feel code is much simpler to read now and still passes all tests

--- lib/factor.rb
+++ lib/factor.rb
@@ -1,22 +1,14 @@
 class Factor
 
   def check_number(n)
-    if plingPlangPlong?(n)
-      "PlingPlangPlong"
-    elsif plangPlong?(n)
-      "PlangPlong"
-    elsif plingPlong?(n)
-      "PlingPlong"
-    elsif plingPlang?(n)
-      "PlingPlang"
-    elsif pling?(n)
-      "Pling"
-    elsif plang?(n)
-      "Plang"
-    elsif plong?(n)
-      "Plong"
-    else
+    output = ""
+    output << "Pling" if pling?(n)
+    output << "Plang" if plang?(n)
+    output << "Plong" if plong?(n)
+    if output.empty?
       "#{n}"
+    else
+      output
     end
   end
 
@@ -34,19 +26,4 @@ class Factor
     n % 7 == 0
   end
 
-  def plingPlang?(n)
-    n % 15 == 0
-  end
-
-  def plingPlong?(n)
-    n % 21 == 0
-  end
-
-  def plangPlong?(n)
-    n % 35 == 0
-  end
-
-  def plingPlangPlong?(n)
-    n % 105 == 0
-  end
 end
